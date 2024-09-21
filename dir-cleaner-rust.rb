bdd58c2904d30a30e099614ac615a89edef2604a
Mohammed Mubarak Ramadan <35968814+mubarak-ramadan@users.noreply.github.com>
2024-09-21 15:31:04 +0100

    homebrew formula fixes

--- dir-cleaner-rust.rb
+++ dir-cleaner-rust.rb
@@ -4,10 +4,12 @@ class DirCleanerRust < Formula
   url "https://github.com/yahayaohinoyi/dir-cleaner-rust/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "f19566a1c25da26e43c649144be1aa962932555ed6c3cfd2fd87f2b456a7aa5f"
   version "1.0.0"
+  depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--root", prefix, "--path", "."
-  end
+    system "cargo", "build", "--release"
+    bin.install "target/release/dir-cleaner-rust"
+   end
 
   test do
     system "#{bin}/dir-cleaner-rust", "--version"
