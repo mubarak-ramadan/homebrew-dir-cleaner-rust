f86b1c46b7e821d210bb3aa555977a998d567e40
Mohammed Mubarak Ramadan <35968814+mubarak-ramadan@users.noreply.github.com>
2024-09-21 14:51:11 +0100

    homebrew formula sha

--- dir-cleaner-rust.rb
+++ dir-cleaner-rust.rb
@@ -2,7 +2,7 @@ class DirCleanerRust < Formula
   desc "Directory cleaner"
   homepage "https://github.com/yahayaohinoyi/dir-cleaner-rust"
   url "https://github.com/yahayaohinoyi/dir-cleaner-rust/releases/tag/v1.0.0/dir-cleaner-rust"
-  sha256 "3e02f4be221b478d5b1cbd2e6c163cb23c12f002636c0f9112d7fb068b9c2ee9"
+  sha256 "f19566a1c25da26e43c649144be1aa962932555ed6c3cfd2fd87f2b456a7aa5f"
   version "1.0.0"
 
   def install
